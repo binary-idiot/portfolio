d551ca68cdb45080b9ecaca6d038fb299e7f0d11
Jonah Middleton <jonah.middleton@gmail.com>
2018-10-11 05:26:18 -0400

    Create gulp images task
    Add functions for processing and cleaning images.
    Use gulp-responsive to resize images for smaller devices.
    Add picture for testing.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,8 @@ import sass       from 'gulp-sass'
 import gulpif     from 'gulp-if'
 import rename     from 'gulp-rename'
 import sourcemaps from 'gulp-sourcemaps'
+import resize     from 'gulp-responsive'
+import size       from 'gulp-size'
 
 import del        from 'del'
 import minimist   from 'minimist'
@@ -22,22 +24,37 @@ const paths = {
 	src: {
 		pages: 'src/pages/',
 		scripts: 'src/scripts/',
-		styles: 'src/styles/'
+		styles: 'src/styles/',
+		images: 'src/images/'
 	},
 	dest: {
 		pages: 'docs/',
 		scripts: 'docs/scripts/',
 		styles: 'docs/styles/',
+		images: 'docs/images/',
 		sourcemaps: '../maps'
 	}
 }
 
-let sassConfig;
+const sizeConfig = {showFiles: true}; 
+
+let sassConfig = {};
+let resizeConfig = {};
+let resizeOptions = {};
 
 if(args.prod){
 	sassConfig = {outputStyle: 'compressed'};
+	resizeConfig = {
+		'*.jpg':{
+				width: 1000,
+				rename: {suffix: '-large'}
+		}
+	}
 }else{
-	sassConfig = {};
+	resizeOptions = {
+		errorOnUnusedImage: false,
+		passThroughUnused: true
+	}
 }
 
 
@@ -54,7 +71,7 @@ if(args.prod){
 			.pipe(connect.reload());
 	}
 
-	const cleanPages = () => {return del([`${paths.dest.pages}**`, `!${paths.dest.pages.slice(0,-1)}`, `!${paths.dest.pages}CNAME`,`!${paths.dest.scripts}**`, `!${paths.dest.styles}**`])}
+	const cleanPages = () => {return del([`${paths.dest.pages}**`, `!${paths.dest.pages.slice(0,-1)}`, `!${paths.dest.pages}CNAME`,`!${paths.dest.scripts}**`, `!${paths.dest.styles}**`, `!${paths.dest.images}**`])}
 
 	const pages = gulp.series(cleanPages, compilePages);
 	pages.description = 'Clean and Compile pages';
@@ -76,7 +93,23 @@ if(args.prod){
 	styles.description = 'Clean and compile styles';
 
 
-const compile = gulp.parallel(pages, styles);
+	// Images 
+	const compileImages = () => {
+		return gulp.src(`${paths.src.images}*`)
+			.pipe(size(sizeConfig))
+			.pipe(resize(resizeConfig, resizeOptions))
+			.pipe(size(sizeConfig))
+			.pipe(gulp.dest(paths.dest.images))
+			.pipe(connect.reload())
+	}
+
+	const cleanImages = () => {return del([`${paths.dest.images}`])}
+
+	const images = gulp.series(cleanImages, compileImages);
+	images.description = 'Clean and resize images'
+
+
+const compile = gulp.parallel(pages, styles, images);
 compile.description = 'Compile all sources';
 
 
@@ -91,7 +124,12 @@ const watchStyles = () => {
 		.on('all', gulp.series(styles));
 }
 
-const watch = gulp.parallel(watchPages, watchStyles);
+const watchImages = () => {
+	return gulp.watch(`${paths.src.images}`)
+			.on('all', gulp.series(images))
+}
+
+const watch = gulp.parallel(watchPages, watchStyles, watchImages);
 watch.description = 'Watch for changes to sources';
 
 
@@ -110,6 +148,8 @@ const defaultTasks = gulp.parallel(serve, watch);
 // Exports
 export {
 	compile,
+	styles,
+	images,
 	pages,
 	watch,
 	serve,
